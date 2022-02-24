fac87aace673e3908539cedbbf3ce5d051712b7e
Konrad Małocha <konrad_malocha@hotmail.com>
2022-02-24 15:28:39 +0100

    TG-7 #done : Implement landing with buttons

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../components/app_button.dart';
+import '../constants/app_colors.dart' as app_colors;
 
 class LandingScreen extends StatelessWidget {
   const LandingScreen({Key? key}) : super(key: key);
@@ -9,17 +12,48 @@ class LandingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AppButton('Logowanie', () {
-              print('logowanie');
-            }),
-            const SizedBox(height: 30),
-            AppButton('Rejestracja', () {
-              print('rejestracja');
-            })
-          ],
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 140, horizontal: 15), // todo : make the border responsive
+          child: DottedBorder(
+            strokeWidth: 1,
+            dashPattern: const [6 , 9],
+            color: app_colors.textColor,
+            borderType: BorderType.RRect,
+            radius: const Radius.circular(40.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AppButton('Logowanie', () {
+                  print('Login');
+                }),
+                const SizedBox(height: 30),
+                AppButton('Rejestracja', () {
+                  print('Registration');
+                }),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        IconButton(
+                            iconSize: 40.0,
+                            color: app_colors.textColor,
+                            onPressed: () {
+                              print('Apple icon');
+                            },
+                            icon: const FaIcon(FontAwesomeIcons.apple)),
+                        IconButton(
+                            iconSize: 40.0,
+                            color: app_colors.textColor,
+                            onPressed: () {
+                              print('Google icon');
+                            },
+                            icon: const FaIcon(FontAwesomeIcons.google))
+                      ]),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
